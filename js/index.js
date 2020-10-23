666d67b17cf9cab42c644f61b48442e9f9e0f568
Igor Ionasku <ionaigor10@gmail.com>
2020-10-23 18:47:42 +0300

    update task

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
+import arr from "./gallery-items.js";
 
+// Создание и рендер разметки по массиву данных и предоставленному шаблону.
+const listRef = document.querySelector(".js-gallery");
+
+const createElementGallery = (arr) =>
+  arr.forEach((element) => {
+    const tagLi = document.createElement("li");
+    tagLi.classList.add("gallery__item");
+
+    const tagA = document.createElement("a");
+    tagA.classList.add("gallery__link");
+    tagA.href = element.original;
+
+    const tagImg = document.createElement("img");
+    tagImg.classList.add("gallery__image");
+    tagImg.src = element.preview;
+    tagImg.dataset.source = element.original;
+    tagImg.alt = element.description;
+
+    listRef.appendChild(tagLi).appendChild(tagA).appendChild(tagImg);
+  });
+
+createElementGallery(arr);
+
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+listRef.addEventListener("click");
